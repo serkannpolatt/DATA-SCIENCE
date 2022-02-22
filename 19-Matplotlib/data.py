de259f5b2be34a7e5d8f6459a536297591d3d915
serkannpolatt <pserkan12@gmail.com>
2022-02-22 19:01:01 +0300

    serkannpolatt

---
+++ 19-Matplotlib/data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+
+#### uyarıları kuralım
+import warnings
+warnings.filterwarnings("ignore")
+
+
+### veriyi aktar 
+df=pd.read_csv("data1.csv")
+
+
+
